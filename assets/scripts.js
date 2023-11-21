4d32dc06cac4945454731913ad63e0fbdc268df0
Cyprien <cres.cyprien@gmail.com>
2023-11-21 14:29:14 +0100

    Dev

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,11 +13,3 @@ $(document).ready(function() {
         tagsPosition: 'top'
     });
 });
-
-var link = document.createElement('link');
-link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css';
-link.rel = 'stylesheet';
-link.onload = function() {
- // Code à exécuter une fois que Bootstrap est chargé
-};
-document.head.appendChild(link);
